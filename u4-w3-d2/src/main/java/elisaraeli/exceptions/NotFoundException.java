72a03278b6d47dda196327eae428cab451db2670
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-01-20 16:18:45 +0100

    adds custom exception

---
+++ u4-w3-d2/src/main/java/elisaraeli/exceptions/NotFoundException.java
@@ -0,0 +1,14 @@
+package elisaraeli.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(long id) {
+        super("Il record con id " + id + " non è stato trovato!");
+    }
+}
+
+
+
+
+
+
+
